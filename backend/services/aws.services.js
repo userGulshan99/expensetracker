80fbf2694a2940c979336e6d6e5f5fde48892a60
Gulshan <bankargulshan@gmail.com>
2025-04-19 19:21:40 +0530

    made some changes in aws services

--- backend/services/aws.services.js
+++ backend/services/aws.services.js
@@ -1,37 +1,31 @@
-const AWS = require('aws-sdk');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
-async function uploadToS3(data, filename, Bucket= 'appexpensetracker'){
+async function uploadToS3(data, filename, Bucket = 'appexpensetracker') {
     try {
-        let s3bucket = new AWS.S3({
-            accessKeyId : process.env.AWS_ACCESS_KEY,
-            secretAccessKey : process.env.AWS_ACCESS_SECRET,
+        const s3Client = new S3Client({
+            region: 'ap-south-1',
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY,
+                secretAccessKey: process.env.AWS_ACCESS_SECRET,
+            },
         });
-    
-        const params = {
-            Bucket : Bucket,
-            Key : filename,
-            Body : data,
-            ACL : 'public-read'
-        };
-    
-        return new Promise((resolve, reject)=>{
-    
-            s3bucket.upload(params, (err, s3responseData)=>{
-                if(err){
-                    reject(err);
-                }else{
-                    const location = s3responseData.Location;
-                    
-                    resolve(location);
-                }
-            })
-        })
-    } catch (error) {
-        return error;
-    }
 
+        const command = new PutObjectCommand({
+            Bucket,
+            Key: filename,
+            Body: data,
+            ACL: 'public-read', // Make sure your bucket policy allows this
+        });
+
+        await s3Client.send(command);
+
+        const location = `https://${Bucket}.s3.ap-south-1.amazonaws.com/${filename}`;
+        return location;
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
     uploadToS3
-}
+};
